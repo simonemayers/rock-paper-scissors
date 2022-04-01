864829779e3c940833ea43f7f04558f28c99879d
Simone Mayers <53239742+simonemayers@users.noreply.github.com>
2022-04-01 14:40:03 -0400

    done

--- script.js
+++ script.js
@@ -21,54 +21,6 @@ let sara = {
     getHandMethod: getHand
 }
 
-function playRound(p1, p2){
-    let h1 = hands[p1.getHandMethod()];
-    let h2 = hands[p2.getHandMethod()];
-    console.log(`${p1.name} played ${h1}. `, `${p2.name} played ${h2}.`)
-    if((h1 === "rock" && h2 === "paper")||(h1 === "paper" && h2 === "scissors") || (h1==="scissors" && h2 === "rock")){
-        console.log(`${h2} beats ${h1}, ${p2.name} wins!`)
-        return p2; 
-    } else if((h2 === "rock" && h1 === "paper")||(h2 === "paper" && h1 === "scissors") || (h2==="scissors" && h1 === "rock")){
-        console.log(`${h1} beats ${h2}, ${p1.name} wins!`)
-        return p1; 
-    } else {
-        console.log("It's a tie"); 
-        return null; 
-    }
-}
-// playRound(simone, sue)
-
-let p1Wins = 0; 
-let p2Wins = 0; 
-function playGame(player1, player2, playUntil=0){
-    let result = playRound(player1, player2)
-    if(result === player1){
-        p1Wins += 1; 
-    } else if(result === player2){
-        p2Wins += 1; 
-    } 
-    if(p1Wins === 5 || p2Wins === 5){
-        playUntil = 5
-        p1Wins = 0; 
-        p2Wins = 0; 
-        return result;
-    }
-    return playGame(player1, player2)
-
-    }
-// playGame(simone, sue)
-
-function playTournament(player1, player2, player3, player4, playUntil = 0){
-    let champ1 = playGame(player1, player2);
-    console.log(`Wow! What a game! ${champ1.name} won this round and made it to the finals!`)
-    let champ2 = playGame(player3, player4);
-    console.log(`Wow! What a game! ${champ2.name} won this round and made it to the finals!`)
-    let winner = playGame(champ1, champ2);
-    console.log(`There you have it guys! World Wide Rock Paper Scissors Champion ${winner.name}! Congratulations! ${winner.name} is the world champion.`)
-    return winner
-}
-
-
 let singleButton = document.querySelector("#single")
 let tournamentButton = document.querySelector("#tournament")
 
@@ -124,14 +76,14 @@ function createGameScreen(){
 createGameScreen()
 let playButton = document.querySelector(".play-game-button")
 
-function playGame(){
+function startGame(){
     playButton.style.display = "none"
 
 }
 
-playButton.addEventListener("click", playGame)
+playButton.addEventListener("click", startGame)
 
-playGame()
+startGame()
 
 function createPlayerContainers(){
     let player1Container = document.createElement("div")
@@ -144,27 +96,91 @@ function createPlayerContainers(){
 createPlayerContainers()
 
 
-let rock1 = document.createElement("p")
-rock1.innerHTML = "Rock"
-rock1.classList.add("rock")
-let paper1 = document.createElement("p")
-paper1.innerHTML = "Paper"
-paper1.classList.add("paper")
-let scissors1 = document.createElement("p")
-scissors1.innerHTML = "Scissors"
-scissors1.classList.add("scissors")
-
-let rock2 = document.createElement("p")
-rock2.innerHTML = "Rock"
-rock2.classList.add("rock")
-let paper2 = document.createElement("p")
-paper2.innerHTML = "Paper"
-paper2.classList.add("paper")
-let scissors2 = document.createElement("p")
-scissors2.innerHTML = "Scissors"
-scissors2.classList.add("scissors")
-
-document.querySelector(".player1-container").appendChild(rock1)
-document.querySelector(".player2-container").appendChild(rock2)
 
 
+let rpsText1 = document.createElement("p")
+rpsText1.classList.add("rps-text")
+rpsText1.id = "rps-text1"
+let player1Container = document.querySelector(".player1-container")
+
+let rpsText2 = document.createElement("p")
+rpsText2.classList.add("rps-text")
+rpsText2.id = "rps-text2"
+let player2Container = document.querySelector(".player2-container")
+
+player1Container.appendChild(rpsText1)
+player2Container.appendChild(rpsText2)
+
+let displayRPS = (weapon) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            Array.from(document.querySelectorAll(".rps-text")).map(m => m.innerHTML = weapon)
+            resolve()
+        }, 2000)
+    })
+}
+
+displayRPS("Rock")
+.then(() => displayRPS("Paper"))
+.then(() => displayRPS("Scissors"))
+.then(() => displayRPS(""))
+.then(() => {
+    // let shootText = document.createElement("h2")
+    // document.querySelector(".game-container").prepend(shootText)
+    // shootText.classList.add("shoot-text")
+    // shootText.innerHTML = "SHOOT"
+    let roundResult = playRound(simone, sue)
+})
+
+console.log(roundResult)
+
+let p1RoundResult = document.createElement("p")
+p1RoundResult.classList.add("round-result")
+document.querySelector(".player1-container").appendChild(p1RoundResult)
+
+function playRound(p1, p2){
+    let h1 = hands[p1.getHandMethod()];
+    let h2 = hands[p2.getHandMethod()];
+    console.log(`${p1.name} played ${h1}. `, `${p2.name} played ${h2}.`)
+    if((h1 === "rock" && h2 === "paper")||(h1 === "paper" && h2 === "scissors") || (h1==="scissors" && h2 === "rock")){
+        p1RoundResult.innerHTML = `${h2} beats ${h1}, ${p2.name} wins!`
+        return p2; 
+    } else if((h2 === "rock" && h1 === "paper")||(h2 === "paper" && h1 === "scissors") || (h2==="scissors" && h1 === "rock")){
+        console.log(`${h1} beats ${h2}, ${p1.name} wins!`)
+        return p1; 
+    } else {
+        console.log("It's a tie"); 
+        return null; 
+    }
+}
+// playRound(simone, sue)
+
+let p1Wins = 0; 
+let p2Wins = 0; 
+function playGame(player1, player2, playUntil=0){
+    let result = playRound(player1, player2)
+    if(result === player1){
+        p1Wins += 1; 
+    } else if(result === player2){
+        p2Wins += 1; 
+    } 
+    if(p1Wins === 5 || p2Wins === 5){
+        playUntil = 5
+        p1Wins = 0; 
+        p2Wins = 0; 
+        return result;
+    }
+    return playGame(player1, player2)
+
+    }
+// playGame(simone, sue)
+
+function playTournament(player1, player2, player3, player4, playUntil = 0){
+    let champ1 = playGame(player1, player2);
+    console.log(`Wow! What a game! ${champ1.name} won this round and made it to the finals!`)
+    let champ2 = playGame(player3, player4);
+    console.log(`Wow! What a game! ${champ2.name} won this round and made it to the finals!`)
+    let winner = playGame(champ1, champ2);
+    console.log(`There you have it guys! World Wide Rock Paper Scissors Champion ${winner.name}! Congratulations! ${winner.name} is the world champion.`)
+    return winner
+}
